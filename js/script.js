07aad6e598679329103c7811cc5959314a9b435a
gabriel-shanahan <41175126+gshanahan@users.noreply.github.com>
2026-04-27 14:02:23 -0400

    more work

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 // Future use: image lazy loading, gallery filtering, animations
 console.log("Overlook Photography loaded");
+
+function openLightbox(card) {
+  const img = card.querySelector("img");
+  const exif = card.querySelector(".exif-bar").innerText;
+
+  document.getElementById("lightbox-img").src = img.src;
+  document.getElementById("lightbox-exif").innerText = exif;
+
+  document.getElementById("lightbox").style.display = "flex";
+}
+
+function closeLightbox() {
+  document.getElementById("lightbox").style.display = "none";
+}
